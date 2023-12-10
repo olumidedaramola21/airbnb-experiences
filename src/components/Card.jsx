4646e7d1a738762a4dc1e1fa5e2ea7d1db5b811a
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-12-10 13:58:09 +0100

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
-import picture from '../images/picture1.png'
-import star from '../images/star 1.png'
+import picture from "../images/picture1.png";
+import star from "../images/star 1.png";
 
 export default function Card() {
-    return (
-        <div className="card--image">
-            <img src={picture} alt="" className='card--image'/>
-            <div className='card--stats'>
-                <img src={star} alt="" />
-                <span>5.0</span>
-                <span>(6) . </span>
-                <span>USA</span>
-                <p className='card--location'>Life lessons with Katie Zaferes</p>
-                <p className='card--price'>From $136 / person</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="card--image">
+      <img src={picture} alt="" className="card--image" />
+      <div className="card--stats">
+        <img src={star} alt="" className="card--star" />
+        <span>5.0</span>
+        <span className="gray">(6) . </span>
+        <span className="gray">USA</span>
+      </div>
+      <p >Life lessons with Katie Zaferes</p>
+      <p><span className="bold">From $136</span> / person</p>
+    </div>
+  );
 }

19c0ae5005243541affa9296b7867d95ef27bc37
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-12-10 13:58:43 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+// import Hero from './components/Hero'
+import Card from './components/Card'
 
 function App() {
     return (
       <div className='app--container'>
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
+      <Card />
       </div>
     )
 }

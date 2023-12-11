3b6c5add90ce8d1272538f680540d55a13ab1e4c
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-12-11 15:01:30 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar";
 // import Hero from './components/Hero'
-import Card from './components/Card'
-
+import Card from "./components/Card";
 
 function App() {
-    
-
-    return (
-      <div className='app--container'>
+  return (
+    <div className="app--container">
       <Navbar />
       {/* <Hero /> */}
-      <Card img='/picture1.png' rating="5.0" reviewCount={"(6)"} country="USA" title="Life lessongs with Katie Zaferes" price={136}/>
-      </div>
-    )
+      <Card
+        img="/picture1.png"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessongs with Katie Zaferes"
+        price={136}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;

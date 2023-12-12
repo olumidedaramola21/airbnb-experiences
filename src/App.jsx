267e59e78621abe8ac6659a44a92f67100095323
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-12-12 17:15:26 +0100

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Hero from './components/Hero'
+import Hero from "./components/Hero";
 import Card from "./components/Card";
 import data from "../data.js";
 
-console.log(data)
+console.log(data);
 
 function App() {
-    const cards = data.map(item => {
-      return (
-        <Card 
-        key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        country={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
-        />
-      )
-    })
-
+  const cards = data.map((item) => {
+    return <Card key={item.id} item={item} />;
+  });
 
   return (
     <div className="app--container">
       <Navbar />
       <Hero />
-      <section className="cards--list">
-      {cards}
-      </section>
+      <section className="cards--list">{cards}</section>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,41 @@
 import PropTypes from "prop-types";
 
 export default function Card(props) {
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
-        {props.openSpots === 0 &&  <div className="card--badge">SOLD OUT</div>}
-        <img src={props.img} alt="Card Image" className="card--image" />
-        <div className="card--stats">
-          <img src="/Star 1.png" alt="" className="card--star" />
-          <span>{props.rating}</span>
-          <span className="gray">({props.reviewCount}) . </span>
-          <span className="gray">{props.country}</span>
-        </div>
-        <p className="card--title">{props.title}</p>
-        <p className="card--prize">
-          <span className="bold">From ${props.price}</span> / person
-        </p>
-        
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={props.item.coverImg} alt="Card Image" className="card--image" />
+      <div className="card--stats">
+        <img src="/Star 1.png" alt="" className="card--star" />
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) . </span>
+        <span className="gray">{props.item.location}</span>
+      </div>
+      <p className="card--title">{props.item.title}</p>
+      <p className="card--prize">
+        <span className="bold">From ${props.item.price}</span> / person
+      </p>
     </div>
   );
 }
 
 Card.propTypes = {
-  img: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  country: PropTypes.string.isRequired,
-  reviewCount: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  openSpots: PropTypes.number.isRequired,
+  item: PropTypes.shape({
+    coverImg: PropTypes.string.isRequired,
+    openSpots: PropTypes.number.isRequired,
+    location: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      rating: PropTypes.number.isRequired,
+      reviewCount: PropTypes.string.isRequired,
+    }).isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
 };
